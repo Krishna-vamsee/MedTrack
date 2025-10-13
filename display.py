b8594ffcdd667e1531c74c76f2eefd65df7dbbbb
Krishna-vamsee <muddapukrishnavamsee@gmail.com>
2025-10-13 18:24:36 +0530

    Update display.py

--- display.py
+++ display.py
@@ -8,6 +8,7 @@ sample_medicines = [
     {"name": "Multivitamin", "dosage": "500mg", "time": "06:30"},
     {"name": "Aspirin", "dosage": "75mg", "time": "07:00"},
     {"name": "Metformin", "dosage": "500mg", "time": "08:00"},
+    {"name": "Dolo", "dosage": "3mg", "time": "8:300"},
     {"name": "Vitamin D", "dosage": "1000 IU", "time": "12:00"},
     {"name": "Fish Oil", "dosage": "1000mg", "time": "13:00"},
     {"name": "Calcium", "dosage": "500mg", "time": "17:00"},
